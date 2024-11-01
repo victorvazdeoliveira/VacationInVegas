64d17a26398561178f4cb847fb14c8315fb23f03
Victor Oliveira <70809637+victorvazdeoliveira@users.noreply.github.com>
2024-10-31 21:56:34 -0300

    Initial Commit

---
+++ VacationInVegas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VacationInVegas
+//
+//  Created by Victor Vaz de Oliveira on 31/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VacationInVegas/VacationInVegasApp.swift
@@ -0,0 +1,17 @@
+//
+//  VacationInVegasApp.swift
+//  VacationInVegas
+//
+//  Created by Victor Vaz de Oliveira on 31/10/24.
+//
+
+import SwiftUI
+
+@main
+struct VacationInVegasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
